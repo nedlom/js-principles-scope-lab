45dbe7661b9b0ef34e10f784449f19ac79c196a3
Nick <nelom3@gmail.com>
2021-09-07 08:03:16 -0700

    commit tree 538416fe2023d81c867c9a905d56a75723d6319d

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = "bob";
+const leastFavoriteCustomer = "initial value"
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+  return customerName;
+}
+
+function setBestCustomer() {
+  bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer(customer) {
+  bestCustomer = customer;
+}
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "changes";
+}
